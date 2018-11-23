08f88d0cd52baa45803ceda34ccb2a92cbf1a632
bbommeri <bbommeri@DIN73003135.corp.capgemini.com>
2018-11-23 16:02:18 +0530

    commit

--- day1top/src/main/java/day1top/Main.java
+++ day1top/src/main/java/day1top/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	System.out.println("heyy broo...welcome");
-	System.out.println("wt hpnd broo.. no reply??");
+	System.out.println("wt hpnd broo.. no reply?");
 
 	}
 
